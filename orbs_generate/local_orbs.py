f46bb07817371920d1ca6d55c3b8f0e52ad5a139
iswhy <94644748+iswhy@users.noreply.github.com>
2023-11-16 12:21:05 -0800

    1) Added a new localization method, manual localization.

--- orbs_generate/local_orbs.py
+++ orbs_generate/local_orbs.py
@@ -4,6 +4,11 @@ from functools import reduce
 from util_orbs import sort_orbs
 
 
+
+loc_type_err = 'localize: The value of the argument \'loc_type\' is undefined, ' + \
+               'The available options are \'ER\', \'PM\', \'B\', and a numpy ' + \
+               'array.'
+
 ##########################################################################
 def localize(orbs, mol, loc_type='PM', loc_irrep=True, rdm_mo=None):
     '''
@@ -20,9 +25,15 @@ def localize(orbs, mol, loc_type='PM', loc_irrep=True, rdm_mo=None):
     '''
     
     print('>>> Performing localization <<<')
-    if loc_type == 'PM': loc_type_ = 'Pipek-Mezey'
-    if loc_type == 'ER': loc_type_ = 'Edmiston-Ruedenberg'
-    if loc_type == 'B': loc_type_ = 'Boys'        
+    if isinstance(loc_type, str):
+        if loc_type == 'PM': loc_type_ = 'Pipek-Mezey'
+        elif loc_type == 'ER': loc_type_ = 'Edmiston-Ruedenberg'
+        elif loc_type == 'B': loc_type_ = 'Boys'
+        else: raise ValueError(loc_type_err)
+    elif isinstance(loc_type, np.ndarray):
+        loc_type_ = 'Manual'
+    else:
+        raise ValueError(loc_type_err)
     print('Localization type = %s' % loc_type_)
 
     if rdm_mo is not None:
@@ -62,16 +73,23 @@ def localize_sub(orbs, mol, loc_type='PM', loc_irrep=True):
         orb_sym_ = orb_sym
         
     #==== Perform localization within each irrep ====#
-    if loc_type == 'ER':
-        do_loc = lo.ER
-    elif loc_type == 'PM':
-        do_loc = lo.PM
-    elif loc_type == 'B':
-        do_loc = lo.Boys
+    if isinstance(loc_type, str):
+        if loc_type == 'ER':
+            do_loc = lo.ER
+        elif loc_type == 'PM':
+            do_loc = lo.PM
+        elif loc_type == 'B':
+            do_loc = lo.Boys
+        else:
+            raise ValueError(loc_type_err)
+    elif isinstance(loc_type, np.ndarray):
+        do_loc = loc_type.copy()
+        assert do_loc.shape[0] == do_loc.shape[1]     # loc_type must be a square matrix.
+        itest = do_loc.T @ do_loc
+        assert abs(np.sum(itest) - do_loc.shape[0]) < 1.0E-12       # Columns of loc_type must be orthonormal.
     else:
-        raise ValueError('localize: The value of the argument \'loc_type\' is undefined, ' + \
-                         'loc_type = ' + str(loc_type) + '. The available options are ' + \
-                         '\'ER\', \'PM\', and \'B\'.')
+        raise ValueError(loc_type_err)
+    
     orbs_l = []
     if loc_irrep:
         #== Loop over the large/small occupation sections ==#
@@ -82,10 +100,16 @@ def localize_sub(orbs, mol, loc_type='PM', loc_irrep=True):
         for s in symset:
             ids = [k for k in range(0,n) if orb_sym_[k]==s]
             orbs_s = orbs_[:, ids]
-            orbs_l.append( do_loc(mol, orbs_s).kernel() )
+            if loc_type == 'ER' or loc_type == 'PM' or loc_type == 'B':
+                orbs_l.append( do_loc(mol, orbs_s).kernel() )
+            elif isinstance(loc_type, np.ndarray):
+                orbs_l.append( orbs_s @ do_loc )
     else:
-        orbs_l.append( do_loc(mol, orbs_).kernel() )
-
+        if loc_type == 'ER' or loc_type == 'PM' or loc_type == 'B':
+            orbs_l.append( do_loc(mol, orbs_).kernel() )
+        elif isinstance(loc_type, np.ndarray):
+            orbs_l.append( orbs_ @ do_loc )
+                
             
     orbs_l = np.hstack(orbs_l)
     return orbs_l
