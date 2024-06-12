e7adbec411989ba59281314f34aad92c6e3f3af4
iswhy <94644748+iswhy@users.noreply.github.com>
2024-06-11 18:48:38 -0700

    1) Added a function that print statistics of the timings from tddmrg output.

--- utils/util_general.py
+++ utils/util_general.py
@@ -121,3 +121,98 @@ def extract_timing(fpath, tinit, dt, tmax=None, substep_thr=1E-10, tthr=1E-12,
             
     return step_cost, tlast
 #######################################################
+
+
+#######################################################
+def timestat(prefix, dt, dfiles, av1=None, av2=None, save_dir='.'):
+    '''
+    dfiles:
+       A dictionary whose element is in the following format
+          <D_string>: [tinit, path0, restart_path1, ...]
+       where D_string is the string signifying the bond dimension, tinit is
+       the initial time for this TDDMRG propagation, path0 is the output 
+       file of the initial TDDMRG simuluation that starts from tinit, and
+       restart_path<n> are the optional restart files if any or if desired.
+    '''
+    
+    step_cost = dfiles.copy()
+    for Ds in step_cost.keys():
+        step_cost[Ds] = []
+    
+    NN = 0
+    for Ds, fpaths in dfiles.items():
+        tinit = fpaths[0]
+        step_cost[Ds] = np.array([])
+        for fpath in fpaths[1:]:
+            print('\nD = ', Ds)
+            print('   File = ', fpath)
+            step_cost0, tmax = util_general.extract_timing(fpath, tinit, dt)
+            print('')
+            if len(step_cost0) > 0:
+                print('   Longest step cost = %.3f' % np.max(step_cost0))
+                print('   Shortest step cost = %.3f' % np.min(step_cost0))
+            print('   Number of recorded time points = ', len(step_cost0))
+            step_cost[Ds] = np.hstack( (step_cost[Ds], np.array(step_cost0)) )
+            tinit = tmax + dt
+    
+        print('')
+        print('Total number of recorded time points = ', len(step_cost[Ds]))
+        print('Average cost per step = %.3f' % np.mean(step_cost[Ds]))
+        print('Standard deviation = %.3f' % np.std(step_cost[Ds]))
+        NN = max(NN, len(step_cost[Ds]))
+                    
+            
+    if av1 is None: av1 = 0
+    if av2 is None: av2 = NN
+    if not os.path.isdir(save_dir): os.mkdir(save_dir)
+    atiming_file = save_dir + '/' + prefix + '.atm'
+    with open(atiming_file, 'w') as tmf:
+        tmf.write(f'Statistics between the {av1:d}-th and {av2:d}-th data series:\n')
+        
+        # column title
+        tmf.write('%11s  ' % 'D')
+        for Ds in step_cost.keys():
+            tmf.write('  %10s' % Ds)
+        tmf.write('\n')
+    
+        # table content
+        tmf.write('%11s  ' % 'Mean')
+        for Ds in step_cost.keys():
+            tmf.write('  %10.3f' % np.mean(step_cost[Ds][av1:av2]))
+        tmf.write('\n')
+        tmf.write('%11s  ' % 'Std. dev.')
+        for Ds in step_cost.keys():
+            tmf.write('  %10.3f' % np.std(step_cost[Ds][av1:av2]))
+        tmf.write('\n')
+        tmf.write('%11s  ' % 'Max')
+        for Ds in step_cost.keys():
+            tmf.write('  %10.3f' % np.max(step_cost[Ds][av1:av2]))
+        tmf.write('\n')
+        tmf.write('%11s  ' % 'Min')
+        for Ds in step_cost.keys():
+            tmf.write('  %10.3f' % np.min(step_cost[Ds][av1:av2]))
+        tmf.write('\n')
+        tmf.write('%11s  ' % 'ndata')
+        for Ds in step_cost.keys():
+            tmf.write('  %10i' % len(step_cost[Ds][av1:av2]))
+        tmf.write('\n')
+            
+    
+    timing_file = save_dir + '/' + prefix + '.tm'
+    with open(timing_file, 'w') as tmf:
+        # column title
+        tmf.write('#%7s  ' % 'No.')
+        for Ds in step_cost.keys():
+            tmf.write('  %10s' % Ds)
+        tmf.write('\n')
+    
+        # table content
+        for i in range(NN):
+            tmf.write('%8i  ' % i)
+            for Ds in step_cost.keys():
+                if i <= len(step_cost[Ds])-1:
+                    tmf.write('  %10.3f' % step_cost[Ds][i])
+                else:
+                    tmf.write('  %10s' % ' ')
+            tmf.write('\n')
+#######################################################
