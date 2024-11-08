754216f1c86ed1652bdf25d6b85c933ab5a391ce
iswhy <94644748+iswhy@users.noreply.github.com>
2024-11-07 19:09:03 -0800

    1) Changed the value of def_te_save_1pdm. 2) Added more readme.

--- defvals.py
+++ defvals.py
@@ -56,7 +56,7 @@ def_n_sub_sweeps_init = 4
 
 def_te_sample = None
 def_te_save_mps = 'overwrite'
-def_te_save_1pdm = False
+def_te_save_1pdm = True
 def_te_save_2pdm = False
 def_save_txt = True
 def_save_npy = True
